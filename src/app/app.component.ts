918cc7919ea3ed10fb6ca6db6e582bcdee4ee98c
Frank Steiner <FrankThomas.Steiner@infineon.com>
2017-11-21 14:34:05 +0100

    updated UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,95 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
+import { NgSwitch } from '@angular/common';
 import 'rxjs/add/operator/map';
+import { Info } from './info';
+import { Veranstaltung } from './veranstaltung';
+
 @Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-message: string = null;
-error: boolean = false;
-constructor(private http: Http) {
-}
-checkInfo() {
-this.http.get('/info').map(response => response.json()).subscribe(info => {
-this.message = info.message;
-this.error = false;
-},
-error => {
-this.error = true;
-this.message = 'ERROR';
-}
-);
-}
+  info: Info = null;
+  allEvents: boolean = false;
+  error: boolean = false;
+
+  events: Veranstaltung[] = null;
+
+  event: Veranstaltung = new Veranstaltung();
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.checkInfo();
+    this.loadEvents();
+  }
+
+  checkInfo() {
+    this.http.get('/v1/info').map(response => response.json()).subscribe(info => {
+      this.info = info;
+      this.error = false;
+    },
+      error => {
+        this.error = true;
+      }
+    );
+  }
+
+  loadEvents() { 
+    this.http.get('/v1/veranstaltungen?allEvents='+this.allEvents).map(response => response.json()).subscribe(veranstaltungen => {
+      this.events = veranstaltungen;
+    }
+    );
+   
+  }
+
+  load(id:number) { 
+    this.http.get('/v1/veranstaltungen/'+id).map(response => response.json()).subscribe(veranstaltung => {
+      this.event = veranstaltung;
+    }
+    );
+   
+  }
+
+  delete(id:number) { 
+    this.http.delete('/v1/veranstaltungen/'+id).subscribe(response => {
+      this.loadEvents();
+      this.event = new Veranstaltung();
+    }
+    );
+   
+  }
+
+  createEvent() {
+    this.http.post('/v1/veranstaltungen', this.event).subscribe(veranstaltungen => {
+      this.loadEvents();
+      this.event = new Veranstaltung();
+    }, error => {
+      window.alert(error);
+    }
+    );
+  } 
+
+  updateEvent() {
+    this.http.put('/v1/veranstaltungen/'+this.event.id, this.event).subscribe(veranstaltungen => {
+      this.loadEvents();
+      this.event = new Veranstaltung();
+    }, error => {
+      window.alert(error);
+    }
+    );
+  }
+  
+  reset() {
+    this.event = new Veranstaltung();    
+  }
+
+  toggleList() {
+    this.allEvents = !this.allEvents;
+    this.loadEvents();
+  }
 }
--- src/app/info.ts
+++ src/app/info.ts
@@ -1,3 +1,3 @@
 export class Info {
-    message:string;
+    message : string;
 }
